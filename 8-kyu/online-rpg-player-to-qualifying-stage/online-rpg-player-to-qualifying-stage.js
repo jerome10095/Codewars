3f558302112760e15eae18c359eb0e137cdb5fc6
jerome10095 <jeromecyiza@gmail.com>
2026-03-27 14:37:34 +0200

    Add solution - CodeHub

---
+++ 8-kyu/online-rpg-player-to-qualifying-stage/online-rpg-player-to-qualifying-stage.js
@@ -0,0 +1,9 @@
+function playerRankUp (points) {
+  let qualifyingPoints = 100;
+if (points >= qualifyingPoints) {
+  return "Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up."
+} else {
+  return false;
+}
+​
+}
