0b2bd924ae81d30dc345e270041f56354c70fd7e
jerome10095 <jeromecyiza@gmail.com>
2026-05-05 13:48:54 +0200

    Add solution - CodeHub

---
+++ 6-kyu/two-sum/two-sum.js
@@ -0,0 +1,11 @@
+function twoSum(numbers, target) {
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+}
+​
+console.log(twoSum([1234, 5678, 9012], 14690));
