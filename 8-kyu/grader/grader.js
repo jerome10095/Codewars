bb7dce3553465ef4bfe38bdd66646b0c776fc4f6
Jerome Cyiza <jeromecyiza@gmail.com>
2026-05-19 14:29:28 +0200

    Add solution - CodeHub

---
+++ 8-kyu/grader/grader.js
@@ -0,0 +1,9 @@
+function grader(score) {
+  return score > 1 || score < 0.6 ? "F" :
+         score >= 0.9 ? "A" :
+         score >= 0.8 ? "B" :
+         score >= 0.7 ? "C" :
+         "D";
+}
+​
+console.log(grader(0.7));
