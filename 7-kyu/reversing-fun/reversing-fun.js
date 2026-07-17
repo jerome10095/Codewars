50832d0d20fe94e68fd029840f6c0a9602563227
Jerome Cyiza <jeromecyiza@gmail.com>
2026-07-17 14:48:05 +0200

    Add solution - CodeHub

---
+++ 7-kyu/reversing-fun/reversing-fun.js
@@ -0,0 +1,14 @@
+function flipNumber(s) {
+  let arr = s.split('');
+  let result = [];
+  
+  while (arr.length > 0) {
+    result.push(arr.pop()); 
+    if (arr.length > 0) {
+      result.push(arr.shift()); 
+    }
+  }
+  
+  return result.join('');
+}
+​
