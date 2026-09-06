4d2c047ba1d4eb69ec73081826ea9f74011c68a2
Jerome Cyiza <jeromecyiza@gmail.com>
2026-09-07 00:01:38 +0200

    Add solution - CodeHub

---
+++ 8-kyu/basic-mathematical-operations/basic-mathematical-operations.js
@@ -0,0 +1,12 @@
+function basicOp(operation, value1, value2) {
+  switch (operation) {
+    case '+': return value1 + value2;
+    case '-': return value1 - value2;
+    case '*': return value1 * value2;
+    case '/': return value1 / value2;
+    default: return 0; // Handles unexpected operations
+  }
+}
+​
+console.log(basicOp('+', 4, 3)); // Outputs: 7
+​
